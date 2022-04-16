ed10573d4de7a2135897bd215a5d64df0025a78a
arunstop <arunstop@gmail.com>
2022-04-17 02:53:54 +0700

    ADDED clear functionality in filter

--- components/sections/main/MainSectionFilter.tsx
+++ components/sections/main/MainSectionFilter.tsx
@@ -3,6 +3,13 @@ import React from "react";
 import { useCountryContext } from "../../../utils/contexts/country/CountryHook";
 import MainSectionFilterChip from "./MainSectionFilterChip";
 
+interface RenderFilterProps {
+  label: string;
+  list: string[];
+  key: string;
+  clearable: boolean;
+}
+
 function MainSectionFilter() {
   const {
     state: countryState,
@@ -17,6 +24,40 @@ function MainSectionFilter() {
     },
   } = useCountryContext();
   // console.log(filterProps.filterProps.continentList);
+
+  const renderFilter = ({ label, list, key, clearable }: RenderFilterProps) => {
+    return (
+      <div className="grid gap-4 w-full sm:w-fit">
+        <div className="flex gap-2 items-center">
+          {label}
+          <button
+            className={`btn btn-outline btn-error btn-sm rounded-full
+            ${clearable ? "visible" : "invisible"}`}
+            onClick={() => countryAction.clearFilter(key)}
+          >
+            Clear
+          </button>
+        </div>
+        <div className="flex w-full flex-wrap gap-2">
+          {list.map((item, idx) => (
+            <MainSectionFilterChip
+              key={idx}
+              text={item}
+              checked={(filters as any)[key].includes(item)}
+              onClick={(status) => {
+                countryAction.setFilter({
+                  key: key,
+                  value: item,
+                  add: status,
+                });
+              }}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="flex flex-wrap gap-4 p-8">
       <div className="grid gap-4 w-full sm:w-fit">
@@ -38,63 +79,24 @@ function MainSectionFilter() {
           </label>
         </div>
       </div>
-      <div className="grid gap-4 w-full sm:w-fit">
-        <label>By Continent : </label>
-        <div className="flex w-full flex-wrap gap-2">
-          {continentList.map((item, idx) => (
-            <MainSectionFilterChip
-              key={idx}
-              text={item}
-              checked={filters.continents.includes(item)}
-              onClick={(status) => {
-                countryAction.setFilter({
-                  key: "continents",
-                  value: item,
-                  add: status,
-                });
-              }}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="grid gap-4 w-full sm:w-fit">
-        <label>By Region :</label>
-        <div className="flex w-full flex-wrap gap-2">
-          {regionList.map((item, idx) => (
-            <MainSectionFilterChip
-              key={idx}
-              text={item}
-              checked={filters.region.includes(item)}
-              onClick={(status) => {
-                countryAction.setFilter({
-                  key: "region",
-                  value: item,
-                  add: status,
-                });
-              }}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="grid gap-4 w-full sm:w-fit">
-        <label>By Subregion :</label>
-        <div className="flex w-full flex-wrap gap-2">
-          {subregionList.map((item, idx) => (
-            <MainSectionFilterChip
-              key={idx}
-              text={item}
-              checked={filters.subregion.includes(item)}
-              onClick={(status) => {
-                countryAction.setFilter({
-                  key: "subregion",
-                  value: item,
-                  add: status,
-                });
-              }}
-            />
-          ))}
-        </div>
-      </div>
+      {renderFilter({
+        label: "By Continents :",
+        list: continentList,
+        clearable: filters["continents"].length !== 0,
+        key: "continents",
+      })}
+      {renderFilter({
+        label: "By Region :",
+        list: regionList,
+        clearable: filters["region"].length !== 0,
+        key: "region",
+      })}
+      {renderFilter({
+        label: "By Subregion :",
+        list: subregionList,
+        clearable: filters["subregion"].length !== 0,
+        key: "subregion",
+      })}
     </div>
   );
 }
