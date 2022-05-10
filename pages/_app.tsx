df910769985b4eab03405d1cb317cef529a819b3
arunstop <arunstop@gmail.com>
2022-05-10 20:52:51 +0700

    FIXED progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from "next/app";
-import NextNProgress from "nextjs-progressbar";
 import LinearProgress from "../components/LinearProgress";
 import ScrollToTopButton from "../components/ScrollToTopButton";
 import "../styles/globals.css";
@@ -7,27 +6,6 @@ import { CountryProvider } from "../utils/contexts/country/CountryProvider";
 import { UiProvider } from "../utils/contexts/ui/UiProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // const router = useRouter();
-  // const [navigating, setNavigating] = useState(false);
-
-  // function onRoutheChangeStart() {
-  //   setNavigating(true);
-  // }
-
-  // function onRoutheChangeComplete() {
-  //   setNavigating(false);
-  // }
-
-  // useEffect(() => {
-  //   router.events.on("routeChangeStart", onRoutheChangeStart);
-  //   // router.events.on("hashChangeStart", onRoutheChangeStart);
-  //   router.events.on("routeChangeComplete", onRoutheChangeComplete);
-  //   return () => {
-  //     router.events.off("routeChangeStart", onRoutheChangeStart);
-  //     router.events.off("routeChangeComplete", onRoutheChangeComplete);
-  //   };
-  // }, [router]);
-
   return (
     <CountryProvider>
       <UiProvider>
